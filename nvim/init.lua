cc3a2ec3675314a5f600a21358a39cf5ee4bd669
Wikash Sewlal <w.sewlal@ret.nl>
2024-02-20 17:10:37 +0100

    Change priority in python linters. Isort will precede black in execution order.

--- nvim/init.lua
+++ nvim/init.lua
@@ -176,8 +176,8 @@ local plugin_none_ls = {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.black,
 				null_ls.builtins.diagnostics.eslint,
 				null_ls.builtins.diagnostics.flake8,
 				-- null_ls.builtins.formatting.spell,
