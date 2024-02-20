7c123af16858a30c65b7fb54125323ea7ae99d62
Wikash Sewlal <w.sewlal@ret.nl>
2024-02-20 13:54:08 +0100

    Add autoformat after saving buffer in nvim.

--- nvim/init.lua
+++ nvim/init.lua
@@ -170,6 +170,9 @@ local plugin_none_ls = {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+		local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+		local event = "BufWritePre" -- or "BufWritePost"
+		local async = event == "BufWritePost"
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
@@ -179,6 +182,24 @@ local plugin_none_ls = {
 				null_ls.builtins.diagnostics.flake8,
 				-- null_ls.builtins.formatting.spell,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.keymap.set("n", "<Leader>f", function()
+						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+					end, { buffer = bufnr, desc = "[LSP] Format file" })
+
+					-- format on save
+					vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+					vim.api.nvim_create_autocmd(event, {
+						buffer = bufnr,
+						group = group,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr, async = async })
+						end,
+						desc = "[LSP] format buffer on save",
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gg=G", vim.lsp.buf.format, { desc = "Format file" })
